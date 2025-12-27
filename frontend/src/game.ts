14606bba6eb9c40ccc51107922f9ad18da56cea0
Sean Pettas <sedp@itu.dk>
2025-12-27 23:00:23 +0100

    added more ts logic

---
+++ frontend/src/game.ts
@@ -0,0 +1,68 @@
+
+
+
+interface GameState {
+    inventory: Record<string, number>;
+    basePrices: Record<string, number>;
+}
+
+
+let gameState: GameState = {
+    //Saves amount players has bought.
+    inventory: {
+        "hands": 0,
+        "loom": 0,
+        "coal-cart": 0,
+        "steam-engine": 0,
+        "factory": 0,
+        "factory-worker": 0,
+        "religion": 0,
+    },
+
+    basePrices: {
+        //Saves basePrice of product when you buy it for the first time
+        "hands": 15,
+        "loom": 100,
+        "coal-cart": 500,
+        "steam-engine": 2500,
+        "factory": 10000,
+        "factory-worker": 50000,
+        "religion": 250000
+    }
+};
+//inventory
+//basePrices
+
+
+
+
+function handlePurchasing(itemName: string): void {
+    const currentAmount = gameState.inventory[itemName];
+    gameState.inventory[itemName] = currentAmount + 1;
+
+    const productElement = document.querySelector(`[data-product="${itemName}"]`);
+
+    if(productElement) {
+        const ownedElement = productElement.querySelector('.product-owned');
+        if (ownedElement) {
+            ownedElement.textContent = String(gameState.inventory[itemName]);
+        }
+        const nextPrice = getNextPrice(itemName);
+        const costElement = productElement.querySelector('.product-cost span');
+        if (costElement) {
+            costElement.textContent = String(nextPrice);
+        }
+    }
+
+
+}
+
+function getNextPrice(itemKey: string): number {
+    const base = gameState.basePrices[itemKey];
+    const amountOwned = gameState.inventory[itemKey];
+
+    // 1.15 is your 15% increase
+    const multiplier = Math.pow(1.15, amountOwned);
+
+    return Math.floor(base * multiplier);
+}
