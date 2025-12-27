98b825652e6eacc608a742f876b0c0b51b9d0f0e
Sean Pettas <sedp@itu.dk>
2025-12-27 20:07:41 +0100

    first commit baby girl

---
+++ src/main/java/com/github/peachy006/storeboughtbackendproper/StoreBoughtBackendProperApplication.java
@@ -0,0 +1,13 @@
+package com.github.peachy006.storeboughtbackendproper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class    StoreBoughtBackendProperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreBoughtBackendProperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/peachy006/storeboughtbackendproper/StoreBoughtBackendProperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.peachy006.storeboughtbackendproper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBoughtBackendProperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
