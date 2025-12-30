aa5a9cbc0e05e70d86a5c147bc0ce12c2cdfbf88
Peachy006 <seanpettas@gmail.com>
2025-12-30 01:48:44 +0100

    switched to java 17 instead of sdk standard 25, also changed maven to stable 3.3.5 version, holy hell this was a nightmare, its cool tho

--- src/main/java/com/github/peachy006/storeboughtbackendproper/StoreBoughtBackendProperApplication.java
+++ src/main/java/com/github/peachy006/storeboughtbackendproper/StoreBoughtBackendProperApplication.java
@@ -2,9 +2,10 @@ package com.github.peachy006.storeboughtbackendproper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class    StoreBoughtBackendProperApplication {
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class StoreBoughtBackendProperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoreBoughtBackendProperApplication.class, args);
---
+++ src/main/java/com/github/peachy006/storeboughtbackendproper/User.java
@@ -0,0 +1,10 @@
+package com.github.peachy006.storeboughtbackendproper;
+
+import jakarta.persistence.*;
+
+
+
+
+public class User {
+
+}
